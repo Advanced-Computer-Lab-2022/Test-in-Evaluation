3f8ad979c5b201fb6b31d7632ffe8e26ef98aa45
Pandemic1617 <43860275+Pandemic1617@users.noreply.github.com>
2022-10-19 20:12:13 +0200

    added change current password

--- server/routes/addRoutes.ts
+++ server/routes/addRoutes.ts
@@ -1,9 +1,11 @@
 import { Express } from "express";
 import { addRoute as loginAddRoute } from "./login";
 import { addRoute as logoutAddRoute } from "./logout";
+import { addRoute as changeCurrentPasswordAddRoute } from "./changeCurrentPassword";
 
 export const addRoutes = (app: Express) => {
     // list addRoutes of routes
     loginAddRoute(app);
     logoutAddRoute(app);
+    changeCurrentPasswordAddRoute(app);
 };
---
+++ server/routes/changeCurrentPassword.ts
@@ -0,0 +1,35 @@
+import { hash, verify } from "argon2";
+import { Express } from "express";
+import { Record, Static, String } from "runtypes";
+import { validateInput } from "../middleware/validateInput";
+import { User } from "../mongo";
+import { Request, Response } from "../types/express";
+import { UserTypes } from "../types/user";
+
+const path = "/api/change_current_password" as const;
+
+const Input = Record({
+    currentPassword: String,
+    newPassword: String,
+});
+
+type Input = Static<typeof Input>;
+
+export const addRoute = (app: Express) => {
+    app.get(path, validateInput(Input), async (req: Request<Input>, res: Response) => {
+        const { currentPassword, newPassword } = req.body;
+        if (!req.session.data.userType) return res.status(400).send({ error: "User not logged in" });
+
+        const user = await User.findOne({ username: req.session.data.username });
+        if (!user) return res.status(400).send({ error: "User not found" });
+
+        const passwordHash = user.passwordHash || "";
+        const isCorrect = await verify(passwordHash, currentPassword);
+
+        if (!isCorrect) return res.status(400).send({ error: "Incorrect password" });
+
+        await user.update({ passwordHash: await hash(newPassword) });
+
+        res.send({ success: true });
+    });
+};
