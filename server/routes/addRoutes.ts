e43d380743442494f3e05c23203486a24f838646
Pandemic1617 <43860275+Pandemic1617@users.noreply.github.com>
2022-10-19 20:28:03 +0200

    added sign up route

--- server/routes/addRoutes.ts
+++ server/routes/addRoutes.ts
@@ -2,10 +2,12 @@ import { Express } from "express";
 import { addRoute as loginAddRoute } from "./login";
 import { addRoute as logoutAddRoute } from "./logout";
 import { addRoute as changeCurrentPasswordAddRoute } from "./changeCurrentPassword";
+import { addRoute as signUpAddRoute } from "./signUp";
 
 export const addRoutes = (app: Express) => {
     // list addRoutes of routes
     loginAddRoute(app);
     logoutAddRoute(app);
     changeCurrentPasswordAddRoute(app);
+    signUpAddRoute(app);
 };
