d38bb65bca0b65c79944b5f0213dcb93d4714792
Pandemic1617 <43860275+Pandemic1617@users.noreply.github.com>
2022-10-19 20:10:43 +0200

    added login and logout routes
    I also moved the default cookie generator to a different file since it is needed in the logout function
    
    # Conflicts:
    #	server/routes/addRoutes.ts

--- server/routes/addRoutes.ts
+++ server/routes/addRoutes.ts
@@ -1,5 +1,9 @@
 import { Express } from "express";
+import { addRoute as loginAddRoute } from "./login";
+import { addRoute as logoutAddRoute } from "./logout";
 
 export const addRoutes = (app: Express) => {
     // list addRoutes of routes
+    loginAddRoute(app);
+    logoutAddRoute(app);
 };
