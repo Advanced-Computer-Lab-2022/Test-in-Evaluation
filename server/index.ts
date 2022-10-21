257057e5892a9999ca177366ae2edbc9447e1c5a
Akram Fahim <akramadel2001@gmail.com>
2022-10-21 17:09:00 +0200

    Implemented issue #12

--- server/index.ts
+++ server/index.ts
@@ -1,2 +1,10 @@
 import "dotenv-safe/config";
-import "./mongo";
+import "./Model";
+import Express from 'express';
+import {getFilteredCourses} from './Controller/getFilteredCourses';
+
+const app = Express();
+app.use(Express.json());
+app.use(Express.urlencoded());
+
+app.get("/courses", getFilteredCourses);
