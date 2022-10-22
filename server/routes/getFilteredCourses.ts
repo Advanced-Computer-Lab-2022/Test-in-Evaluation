17ffebe234486fc7a3a2d07c3beb171c5134563c
Akram Fahim <akramadel2001@gmail.com>
2022-10-22 16:37:47 +0200

    Followed Noureldin's template

--- server/routes/getFilteredCourses.ts
+++ server/routes/getFilteredCourses.ts
@@ -1,6 +1,6 @@
 import { Express } from "express";
 import mongoose from 'mongoose';
-import { Record, Static } from "runtypes";
+import { Record, Static, String, Number, Undefined} from "runtypes";
 import { validateInput } from "../middleware/validateInput";
 import { Request, Response } from "../types/express";
 
@@ -9,18 +9,26 @@ const path = "/api/courses" as const;
 import {CourseSchema} from '../Model/course'
 const Course = mongoose.model('Course', CourseSchema);
 
-
 const Input = Record({
-    title: String,
-    subject: String,
-    summary: String,
-    price: Number,
-    sections: Array(Record({ title: String, description: String, totalHours: Number })),
+    title: String.Or(Undefined),
+    subject: String.Or(Undefined),
+    maxPrice: Number.Or(Undefined),
+    instructor: Number.Or(Undefined),
 });
 type Input = Static<typeof Input>;
 
 export const addRoute = (app: Express) => {
     app.get(path, validateInput(Input), async (req: Request<Input>, res: Response) => {
-        
+        let filters = req.body;
+        let courseFilters = {};
+        if (filters.maxPrice !== undefined)
+            courseFilters = {price: {lte: courseFilters}};
+        if (filters.instructor !== undefined)
+            courseFilters = {...courseFilters, instructor: filters.instructor};
+        if (filters.subject !== undefined)
+            courseFilters = {...courseFilters, subject: filters.subject};
+        if (filters.title !== undefined)
+            courseFilters = {...courseFilters, title: ".*" + filters.title + ".*"};
+        return await Course.find(courseFilters);
     });
 };
