8e6dfa9431d93aa0f9b401338375cb19f044d9f2
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2022-10-30 13:23:02 +0200

    Added bg color

--- client/src/pages/SignUp/SignUpPage.tsx
+++ client/src/pages/SignUp/SignUpPage.tsx
@@ -45,6 +45,7 @@ function SignUpPage() {
                     width: "40%",
                     boxShadow:
                         "inset 0 -3em 3em rgba(0, 0, 0, 0.1), 0 0 0 2px rgb(255, 255, 255), 0.3em 0.3em 1em rgba(0, 0, 0, 0.3)",
+                    backgroundColor: "white",
                 }}
             >
                 <Typography variant="h2" gutterBottom>
