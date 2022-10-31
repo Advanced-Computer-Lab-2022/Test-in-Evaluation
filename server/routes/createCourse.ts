b7925cdbe0b6f38c5dd521e26ec7f1e76c6676a1
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-10-31 21:23:30 +0200

    resolve merge conflicts

--- server/routes/createCourse.ts
+++ server/routes/createCourse.ts
@@ -1,11 +1,7 @@
 import { Express } from "express";
 import { Array, Number, Record, Static, String } from "runtypes";
 import { validateInput } from "../middleware/validateInput";
-<<<<<<< Updated upstream
-import { Course, Section, Subject, User } from "../mongo";
-=======
 import { Course, Section, User, Subject } from "../mongo";
->>>>>>> Stashed changes
 import { Request, Response } from "../types/express";
 import { UserTypes } from "../types/user";
 
@@ -16,32 +12,52 @@ const Input = Record({
     subject: String,
     summary: String,
     price: Number,
-    sections: Array(Record({ title: String, description: String, totalHours: Number })).withConstraint((sections) => sections.length > 0),
+    sections: Array(
+        Record({ title: String, description: String, totalHours: Number })
+    ).withConstraint((sections) => sections.length > 0),
 });
 
 type Input = Static<typeof Input>;
 
 export const addRoute = (app: Express) => {
-    app.post(path, validateInput(Input), async (req: Request<Input>, res: Response) => {
-        if (req.session.data.userType !== UserTypes.instructor) return res.status(400).send({ error: "unauthorized" });
+    app.post(
+        path,
+        validateInput(Input),
+        async (req: Request<Input>, res: Response) => {
+            if (req.session.data.userType !== UserTypes.instructor)
+                return res.status(400).send({ error: "unauthorized" });
 
-        const { price, sections, subject, summary, title } = req.body;
-        const totalHours = sections.map((v) => v.totalHours).reduce((a, b) => a + b);
-        const instructorId = await User.findOne({ username: req.session.data.username }).then((v) => v?._id);
-        const subjectId = subject ? await Subject.findOne({ Name: subject }).then((v) => v?._id) : undefined;
-        const course = await Course.create({ price, subject: subjectId, summary, title, totalHours, instructor: instructorId! });
+            const { price, sections, subject, summary, title } = req.body;
+            const totalHours = sections
+                .map((v) => v.totalHours)
+                .reduce((a, b) => a + b);
+            const instructorId = await User.findOne({
+                username: req.session.data.username,
+            }).then((v) => v?._id);
+            const subjectId = subject
+                ? await Subject.findOne({ Name: subject }).then((v) => v?._id)
+                : undefined;
+            const course = await Course.create({
+                price,
+                subject: subjectId,
+                summary,
+                title,
+                totalHours,
+                instructor: instructorId!,
+            });
 
-        await Promise.all(
-            sections.map(({ description, title, totalHours }) =>
-                Section.create({
-                    description,
-                    name: title,
-                    parentCourse: course._id,
-                    totalHours,
-                })
-            )
-        );
+            await Promise.all(
+                sections.map(({ description, title, totalHours }) =>
+                    Section.create({
+                        description,
+                        name: title,
+                        parentCourse: course._id,
+                        totalHours,
+                    })
+                )
+            );
 
-        return res.send({ ok: true });
-    });
+            return res.send({ ok: true });
+        }
+    );
 };
