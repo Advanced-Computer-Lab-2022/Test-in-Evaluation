0e7c9842c836da7018d993528539480d6f948a81
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2022-10-31 23:45:12 +0200

    Added checkbox

--- client/src/pages/CourseList/CourseList.tsx
+++ client/src/pages/CourseList/CourseList.tsx
@@ -1,10 +1,18 @@
 import { CourseContainer } from "./CourseListStyles";
 import CourseCard from "./CourseCard";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import axios from "axios";
 import { Course } from "../../global";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../App";
+import { Checkbox, FormControlLabel, Typography } from "@mui/material";
 
 const CourseList = () => {
+
+    const [viewMyCoursesOnly, setViewMyCoursesOnly] = useState(false);
+
+    const userState = useContext(UserContext);
+
     const [courses, setCourses] = useState<Course[]>([]);
     const [loading, setLoading] = useState<boolean>(false);
     const getCourses = async () => {
@@ -26,11 +34,16 @@ const CourseList = () => {
     }, []);
     if (loading) return <div>Loading...</div>;
     return (
+        <div>
+        {userState.userType === "instructor" && (<div>
+            <FormControlLabel control={<Checkbox value={viewMyCoursesOnly} onChange={(e, v) => setViewMyCoursesOnly(v)} />} label="View My Courses Only" />
+        </div>)}
         <CourseContainer>
             {courses.map((course) => (
                 <CourseCard course={course} key={course._id} />
             ))}
         </CourseContainer>
+        </div>
     );
 };
 
