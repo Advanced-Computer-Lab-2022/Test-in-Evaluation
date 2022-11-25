c162494b1ffe0d84c919a0c8da9da98ac783b3c4
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-11-25 22:55:36 +0200

    feat: Create a login protected route

--- client/src/components/index.ts
+++ client/src/components/index.ts
@@ -1,3 +1,4 @@
 export { default as NavBar } from "./NavBar/NavBar";
 export { default as SearchBar } from "./SearchBar/SearchBar";
 export { default as Loader } from "./Loader/Loader";
+export { default as LoginProtected } from "./ProtectedRoutes/LoginProtected";
