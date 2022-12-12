55e2ea7aa2e0411349eea1328637fe89bfc58acd
Akram Fahim <akramadel2001@gmail.com>
2022-12-12 19:47:32 +0200

    Added reviewer name in the data return in the course reviews

--- server/routes/getAllReviews.ts
+++ server/routes/getAllReviews.ts
@@ -1,7 +1,7 @@
 import { Express } from "express";
 import { Record, Static, String } from "runtypes";
 import { validateInput } from "../middleware/validateInput";
-import { Review } from "../mongo";
+import { Review, User } from "../mongo";
 import { Request, Response } from "../types/express";
 import { getReviewedObject } from "./writeReview";
 
@@ -21,7 +21,14 @@ export const addRoute = (app: Express) => {
             const { reviewed } = req.body;
             const reviewedObj = await getReviewedObject(reviewed);
             if (!reviewedObj) return res.status(400).send("Invalid Content");
-            res.send(await Review.find({ reviewed }));
+            let reviews = await Review.find({ reviewed });
+            reviews = reviews.map((review: any) => ({...review, reviewerName: getReviewerName(review.reviewer)}));
+            res.send();
         }
     );
 };
+
+async function getReviewerName(reviewer: any) : Promise<string> {
+    const reviewerUser = await User.findById(reviewer);
+    return reviewerUser?.firstName + " " + reviewerUser?.lastName;
+}
