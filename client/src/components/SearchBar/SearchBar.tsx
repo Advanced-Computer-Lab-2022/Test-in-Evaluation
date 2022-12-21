8d714d2e718dbec02a09ecd55091ed327c41bd2b
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-12-21 22:38:18 +0200

    adjust: Add the searchbar submit functionality

--- client/src/components/SearchBar/SearchBar.tsx
+++ client/src/components/SearchBar/SearchBar.tsx
@@ -2,12 +2,15 @@ import { FormEvent } from "react";
 import { Search, SearchIconWrapper, StyledInputBase } from "./SeachBarStyles";
 import SearchIcon from "@mui/icons-material/Search";
 import { useRef } from "react";
+import { useNavigate } from "react-router-dom";
 
 function NavSearchBar() {
+    const navigate = useNavigate();
     const searchRef = useRef<HTMLInputElement>(null);
     const onSubmit = (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault();
         const val = (searchRef?.current?.children[0] as HTMLInputElement).value;
+        navigate("/search?course=" + val);
     };
     return (
         <Search onSubmit={onSubmit}>
