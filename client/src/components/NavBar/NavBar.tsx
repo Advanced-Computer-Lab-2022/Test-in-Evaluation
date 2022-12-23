0677b720df49a42f33a0059ff656815ea9ec0315
Ahmed Elwasefi <ahmad.alwasifi@gmail.com>
2022-12-23 20:58:05 +0200

    Fixed 24hr bug!
    Co-authored-by: Pandemic1617 <43860275+Pandemic1617@users.noreply.github.com>

--- client/src/components/NavBar/NavBar.tsx
+++ client/src/components/NavBar/NavBar.tsx
@@ -21,162 +21,154 @@ import { apiURL, UserContext } from "../../App";
 
 const drawerWidth = 240;
 type NavItem = {
-    text: string;
-    onClick: () => void;
+  text: string;
+  onClick: () => void;
 };
 
 export default function DrawerAppBar() {
-    const navigate = useNavigate();
-    const [mobileOpen, setMobileOpen] = useState(false);
-    const userState = useContext(UserContext);
+  const navigate = useNavigate();
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const userState = useContext(UserContext);
 
-    const handleDrawerToggle = () => {
-        setMobileOpen(!mobileOpen);
-    };
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
 
-    const commonNavItems: NavItem[] = [
-        { text: "Courses", onClick: () => navigate("/courses") },
-        { text: "search", onClick: () => navigate("/search") },
-    ];
-    const loggedInList: NavItem[] = [
-        ...commonNavItems,
-        { text: "Profile", onClick: () => navigate("/profile") },
-        {
-            text: "Logout",
-            onClick: () => {
-                axios
-                    .post(apiURL + "/logout", { withCredentials: true })
-                    .then((response) => {
-                        userState.setLoggedIn(false);
-                        userState.setUserType("none");
-                        navigate("/");
-                    });
-            },
-        },
-    ];
-    const loggedOutList: NavItem[] = [
-        ...commonNavItems,
-        { text: "Sign Up", onClick: () => navigate("/signup") },
-        {
-            text: "Sign In",
-            onClick: () => {
-                navigate("/signin");
-            },
-        },
-    ];
-    const navItems: NavItem[] = userState.loggedIn
-        ? loggedInList
-        : loggedOutList;
+  const commonNavItems: NavItem[] = [
+    { text: "Courses", onClick: () => navigate("/courses") },
+    { text: "search", onClick: () => navigate("/search") },
+  ];
+  const loggedInList: NavItem[] = [
+    ...commonNavItems,
+    { text: "Profile", onClick: () => navigate("/profile") },
+    {
+      text: "Logout",
+      onClick: () => {
+        axios
+          .post(apiURL + "/logout", {}, { withCredentials: true })
+          .then((response) => {
+            userState.setLoggedIn(false);
+            userState.setUserType("none");
+            navigate("/");
+          });
+      },
+    },
+  ];
+  const loggedOutList: NavItem[] = [
+    ...commonNavItems,
+    { text: "Sign Up", onClick: () => navigate("/signup") },
+    {
+      text: "Sign In",
+      onClick: () => {
+        navigate("/signin");
+      },
+    },
+  ];
+  const navItems: NavItem[] = userState.loggedIn ? loggedInList : loggedOutList;
 
-    const drawer = (
-        <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
-            <Typography variant="h6" sx={{ my: 2 }}>
-                LOGO
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        LOGO
+      </Typography>
+      <Divider />
+      <List>
+        {navItems.map((item) => (
+          <ListItem key={item.text} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }} onClick={item.onClick}>
+              <ListItemText primary={item.text} />
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  return (
+    <Box sx={{ display: "flex", marginBottom: "0" }}>
+      <AppBar component="nav" sx={{ px: 3 }}>
+        <Toolbar sx={{ justifyContent: "space-between" }}>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { md: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ display: "flex", flexGrow: 1, gap: 0.5 }}>
+            <Typography
+              variant="h6"
+              sx={{
+                display: { xs: "none", md: "block" },
+                "&:hover": { cursor: "pointer" },
+              }}
+              onClick={() => navigate("/")}
+            >
+              LOGO
             </Typography>
-            <Divider />
-            <List>
-                {navItems.map((item) => (
-                    <ListItem key={item.text} disablePadding>
-                        <ListItemButton
-                            sx={{ textAlign: "center" }}
-                            onClick={item.onClick}
-                        >
-                            <ListItemText primary={item.text} />
-                        </ListItemButton>
-                    </ListItem>
-                ))}
-            </List>
-        </Box>
-    );
-
-    return (
-        <Box sx={{ display: "flex", marginBottom: "0" }}>
-            <AppBar component="nav" sx={{ px: 3 }}>
-                <Toolbar sx={{ justifyContent: "space-between" }}>
-                    <IconButton
-                        color="inherit"
-                        aria-label="open drawer"
-                        edge="start"
-                        onClick={handleDrawerToggle}
-                        sx={{ mr: 2, display: { md: "none" } }}
-                    >
-                        <MenuIcon />
-                    </IconButton>
-                    <Box sx={{ display: "flex", flexGrow: 1, gap: 0.5 }}>
-                        <Typography
-                            variant="h6"
-                            sx={{
-                                display: { xs: "none", md: "block" },
-                                "&:hover": { cursor: "pointer" },
-                            }}
-                            onClick={() => navigate("/")}
-                        >
-                            LOGO
-                        </Typography>
-                        <NavSearchBar />
-                    </Box>
-                    <Box>
-                        <Typography>Country:</Typography>
-                        <Select
-                            value={userState.country}
-                            label="Select Country"
-                            sx={{ background: "white" }}
-                            variant="standard"
-                            onChange={(event) => {
-                                userState.setCountry(event.target.value);
-                            }}
-                        >
-                            {countries.map((element, index) => {
-                                return (
-                                    <MenuItem
-                                        value={element.label}
-                                        key={element.label}
-                                    >
-                                        <img
-                                            loading="lazy"
-                                            width="20"
-                                            src={`https://flagcdn.com/w20/${element.code.toLowerCase()}.png`}
-                                        />{" "}
-                                        &nbsp;
-                                        {element.label}
-                                    </MenuItem>
-                                );
-                            })}
-                        </Select>
-                    </Box>
-                    <Box sx={{ display: { xs: "none", md: "block" } }}>
-                        {navItems.map((item) => (
-                            <Button
-                                key={item.text}
-                                sx={{ color: "#fff" }}
-                                onClick={item.onClick}
-                            >
-                                {item.text}
-                            </Button>
-                        ))}
-                    </Box>
-                </Toolbar>
-            </AppBar>
-            <Toolbar sx={{ p: 1 }} />
-            <Box component="nav">
-                <Drawer
-                    variant="temporary"
-                    open={mobileOpen}
-                    onClose={handleDrawerToggle}
-                    ModalProps={{
-                        keepMounted: true,
-                    }}
-                    sx={{
-                        display: { xs: "block", md: "none" },
-                        "& .MuiDrawer-paper": {
-                            boxSizing: "border-box",
-                            width: drawerWidth,
-                        },
-                    }}
-                >
-                    {drawer}
-                </Drawer>
-            </Box>
-        </Box>
-    );
+            <NavSearchBar />
+          </Box>
+          <Box>
+            <Typography>Country:</Typography>
+            <Select
+              value={userState.country}
+              label="Select Country"
+              sx={{ background: "white" }}
+              variant="standard"
+              onChange={(event) => {
+                userState.setCountry(event.target.value);
+              }}
+            >
+              {countries.map((element, index) => {
+                return (
+                  <MenuItem value={element.label} key={element.label}>
+                    <img
+                      loading="lazy"
+                      width="20"
+                      src={`https://flagcdn.com/w20/${element.code.toLowerCase()}.png`}
+                    />{" "}
+                    &nbsp;
+                    {element.label}
+                  </MenuItem>
+                );
+              })}
+            </Select>
+          </Box>
+          <Box sx={{ display: { xs: "none", md: "block" } }}>
+            {navItems.map((item) => (
+              <Button
+                key={item.text}
+                sx={{ color: "#fff" }}
+                onClick={item.onClick}
+              >
+                {item.text}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </AppBar>
+      <Toolbar sx={{ p: 1 }} />
+      <Box component="nav">
+        <Drawer
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true,
+          }}
+          sx={{
+            display: { xs: "block", md: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+    </Box>
+  );
 }
