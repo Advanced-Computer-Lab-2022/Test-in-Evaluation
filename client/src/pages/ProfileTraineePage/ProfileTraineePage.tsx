23fec4ecdac80abcc392bdb75387f229d1ca4469
Mahmoud Hosam <mahmoudhosam44@gmail.com>
2022-12-25 11:32:01 +0200

    adjust: Change signup page style

--- client/src/pages/ProfileTraineePage/ProfileTraineePage.tsx
+++ client/src/pages/ProfileTraineePage/ProfileTraineePage.tsx
@@ -7,34 +7,38 @@ const ProfileTraineePage = () => {
     const [optionTab, setOptionTab] = useState("reportedProblems");
 
     return (
-        <Container sx={{ minHeight: "100%" }}>
-            <Box
+        <Box
+            sx={{
+                background: "white",
+                display: "flex",
+                height: "100%",
+            }}
+        >
+            <Tabs
                 sx={{
-                    background: "white",
-                    display: "flex",
-                    minHeight: "80vh",
+                    paddingY: "2.5rem",
+                    borderRight: 1,
+                    borderColor: "divider",
+                    boxShadow: "rgba(0, 0, 0, 0.3) 0px 5px 15px",
+                }}
+                value={optionTab}
+                orientation="vertical"
+                onChange={(event, value) => {
+                    setOptionTab(value);
                 }}
             >
-                <Tabs
-                    sx={{
-                        paddingY: "2.5rem",
-                        borderRight: 1,
-                        borderColor: "divider",
-                        boxShadow: "rgba(0, 0, 0, 0.3) 0px 5px 15px",
-                    }}
-                    value={optionTab}
-                    orientation="vertical"
-                    onChange={(event, value) => {
-                        setOptionTab(value);
-                    }}
-                >
-                    <Tab label="Reported Problems" value="reportedProblems" />
-                </Tabs>
-                <Box sx={{ flexGrow: 1, p: 2 }}>
-                    {optionTab === "reportedProblems" && <ReportedProblems />}
-                </Box>
+                <Tab label="Reported Problems" value="reportedProblems" />
+            </Tabs>
+            <Box
+                sx={{
+                    flexGrow: 1,
+                    p: 2,
+                    border: "1px solid #e0e0e0",
+                }}
+            >
+                {optionTab === "reportedProblems" && <ReportedProblems />}
             </Box>
-        </Container>
+        </Box>
     );
 };
 
